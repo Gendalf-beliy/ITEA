e9534bdb939c681adb001263bec612c79b15dcba
Viacheslav Bilous <v.bilous@booongo.com>
2018-10-05 21:24:32 +0300

    1

--- 1/new.py
+++ 1/new.py
@@ -1,1 +1,16 @@
-print('Hello, world')
+def func(str):
+    lst = []
+    for s in str:
+        lst.append(ord(s))
+    return tuple(lst)
+
+print(func('hello'))
+
+
+def test(tpl):
+    str = ''
+    for num in tpl:
+        str += chr(num)
+    return str
+
+print(test(func('hello')))
